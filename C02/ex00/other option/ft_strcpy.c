21ff1479acf0d659b066a73607c5406c6d2e7af0
Mustafa Mujic <mmujic@c2r7s2.42lausanne.ch>
2021-07-14 17:54:43 +0200

    13.07ajoutC02ex00

---
+++ C02/ex00/other option/ft_strcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmujic <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/14 11:20:02 by mmujic            #+#    #+#             */
+/*   Updated: 2021/07/14 17:51:36 by mmujic           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	index;
+
+	index = 0;
+	while (src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char dest[] = "destination";
+	char src[] = "test";
+
+	printf("%s\n", dest);
+	ft_strcpy(dest, src);
+	printf("%s\n", dest);
+}
