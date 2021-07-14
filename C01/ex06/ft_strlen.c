776469e3512ddbe070908d2c6134a52ebea985e6
Mustafa Mujic <mmujic@c2r8s3.42lausanne.ch>
2021-07-14 10:27:44 +0200

    14.07ex06

---
+++ C01/ex06/ft_strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	charnum = 0;
+	while ( *str != '\0')
+	{
+		str++;
+		charnum++;
+	}
+	return (charnum);
+}
+
+int	main(void)
+{
+	int	charnum;
+	char tab[] = "hello";
+	charnum = ft_strlen(&tab[0]);
+	printf("%d\n",charnum );
+}
