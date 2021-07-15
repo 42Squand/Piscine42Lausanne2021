1ae6e9da375150d4ed2eb76d9290ad231ff0a723
Mustafa Mujic <mmujic@c2r9s10.42lausanne.ch>
2021-07-15 17:04:47 +0200

    ex02

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -2,20 +2,30 @@
 
 int	ft_str_is_alpha(char *str)
 {
-	while ( str[] != '\0')
+	while (*str != '\0')
 	{
-		if ((str => 'A' && str <='Z') || (str => 'a' && str <= 'z'))
+		if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
 		{
-		str++;
+			str++;
 		}
-		else(str = 0
-			
-		
+		else
+		{
+			return (0);
+		}
 	}
-		
+	return (1);
 }
+
+		
+
+		
+
 
 int	main(void)
 {
-	
+	int x;
+	char str[] = "";
+
+	x = ft_str_is_alpha(&str[0]);
+	printf("%d\n", x);
 }
