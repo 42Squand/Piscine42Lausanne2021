e76a98580caaac4b436c1e0317e72c6283db491f
Mustafa Mujic <mmujic@c2r9s10.42lausanne.ch>
2021-07-15 18:11:48 +0200

    ex06

---
+++ C02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmujic <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/15 17:48:41 by mmujic            #+#    #+#             */
+/*   Updated: 2021/07/15 17:54:40 by mmujic           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_uppercase(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str >= 'A' && *str <= 'Z')
+		{
+			str++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	int maj;
+	char str[] = "123";
+
+	maj = ft_str_is_uppercase(&str[0]);
+	printf("%d\n", maj);
+}
---
+++ C02/ex06/ft_str_is_printable.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmujic <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/15 18:01:10 by mmujic            #+#    #+#             */
+/*   Updated: 2021/07/15 18:08:01 by mmujic           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_printable(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str >= '!' && *str <= '~' )
+		{
+			str++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	int sc;
+	char str[] = " ! ~";
+
+	sc = ft_str_is_printable(&str[0]);
+	printf("%d\n", sc);
+}
