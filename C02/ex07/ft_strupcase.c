ec6943f5c1b8a867528bc7f6bda05379b038320f
Mustafa Mujic <mmujic@c2r9s10.42lausanne.ch>
2021-07-15 18:40:04 +0200

    ex07

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmujic <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/15 18:23:13 by mmujic            #+#    #+#             */
+/*   Updated: 2021/07/15 18:35:59 by mmujic           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str >= 97 && *str <= 122)
+		{
+			*str = *str - 32;
+		}
+		str++;
+	}
+	return (str);
+}
+
+int	main(void)
+{
+	char desti[] = "desti";
+	char str[] = "salut";
+	
+	ft_strupcase(&str[0]);
+	printf("%s\n", str);
+}	
