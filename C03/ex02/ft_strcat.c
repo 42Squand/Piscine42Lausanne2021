eb2ac49455c696daeb29d5ef63c9c79326feffb6
Mustafa Mujic <mmujic@c2r1s5.42lausanne.ch>
2021-07-19 16:41:26 +0200

    C03ex02

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmujic <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/19 09:59:45 by mmujic            #+#    #+#             */
+/*   Updated: 2021/07/19 16:37:05 by mmujic           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int i;
+	int i2;
+	
+	i = 0;
+	i2 = 0;
+	while(dest[i] != '\0')
+	{
+		i++;
+	}
+	while(src[i2] != '\0')
+	{
+		dest[i] = src[i2];
+		i++;
+		i2++;
+	}
+	return (dest);
+}
+
+int main(void)
+{
+	char dest[] = "salut";
+	char src[] = "ca va";
+
+	ft_strcat(dest, src);
+	printf("%s\n", dest);
+}
