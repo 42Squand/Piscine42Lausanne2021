b25f8d78c5d526b34c999d3360bc029c6cf5a93c
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2022-03-17 01:08:14 +0100

    Update ft_strcpy.c

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmujic <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*   By: mmujic <mmujic@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 11:20:02 by mmujic            #+#    #+#             */
 /*   Updated: 2021/07/15 12:36:34 by mmujic           ###   ########.fr       */
