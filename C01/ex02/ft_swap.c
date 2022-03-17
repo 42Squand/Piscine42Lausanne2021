1e5383db277a97f62cee3b5338e6e185f88bf96c
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2022-03-17 01:05:58 +0100

    Update ft_swap.c

--- C01/ex02/ft_swap.c
+++ C01/ex02/ft_swap.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmujic <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*   By: mmujic <mmujic@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 17:27:15 by mmujic            #+#    #+#             */
 /*   Updated: 2021/07/12 18:05:52 by mmujic           ###   ########.fr       */
