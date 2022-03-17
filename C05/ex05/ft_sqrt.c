db74b6abe3a480a7bfa731d25d568252d518f8be
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2022-03-17 01:14:55 +0100

    Update ft_sqrt.c

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sqrt.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmujic <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*   By: mmujic <mmujic@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 13:38:03 by mmujic            #+#    #+#             */
 /*   Updated: 2021/07/29 13:56:28 by mmujic           ###   ########.fr       */
