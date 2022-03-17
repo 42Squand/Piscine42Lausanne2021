cd5603295a78d1db9f2249bdad7b018cd8e663cb
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2022-03-17 01:04:36 +0100

    Update ft_print_numbers.c

--- C00/ex03/ft_print_numbers.c
+++ C00/ex03/ft_print_numbers.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_numbers.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmujic <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*   By: mmujic <mmujic@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 19:01:42 by mmujic            #+#    #+#             */
 /*   Updated: 2021/07/13 08:56:51 by mmujic           ###   ########.fr       */
